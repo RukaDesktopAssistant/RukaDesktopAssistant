7eea730d28ad7ef2a288c23df839ec44c3290298
RukaDesktopAssistant <mattyakunm@gmail.com>
2026-09-24 02:49:24 +0900

    fix: correct Windows startup registry path and add status check

--- Services/StartupService.cs
+++ Services/StartupService.cs
@@ -4,11 +4,22 @@ namespace RukaDesktopAssistant.Services;
 
 public sealed class StartupService
 {
-    private const string RunKey = @"Software\\Microsoft\\Windows\\CurrentVersion\\Run";
+    private const string RunKey = @"Software\Microsoft\Windows\CurrentVersion\Run";
+
     public void SetEnabled(bool enabled, string executablePath)
     {
-        using var key = Registry.CurrentUser.OpenSubKey(RunKey, writable: true) ?? Registry.CurrentUser.CreateSubKey(RunKey);
-        if (enabled) key.SetValue("RukaDesktopAssistant", $"\"{executablePath}\"");
-        else key.DeleteValue("RukaDesktopAssistant", false);
+        using var key = Registry.CurrentUser.OpenSubKey(RunKey, writable: true)
+            ?? Registry.CurrentUser.CreateSubKey(RunKey);
+
+        if (enabled)
+            key?.SetValue("RukaDesktopAssistant", $"\"{executablePath}\"");
+        else
+            key?.DeleteValue("RukaDesktopAssistant", false);
+    }
+
+    public bool IsEnabled()
+    {
+        using var key = Registry.CurrentUser.OpenSubKey(RunKey, writable: false);
+        return key?.GetValue("RukaDesktopAssistant") is not null;
     }
 }
